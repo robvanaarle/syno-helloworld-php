2b1f6b75d1f229081bab66a556dec244cacc8e3b
Rob van Aarle <robvanaarle@gmail.com>
2014-08-19 19:01:02 +0200

    Replaced application.cfg with ui folder and created a package directory

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# Go to HelloWorldPhp dir, this makes things a lot easier. Otherwise the package
-# and spk contain the folder 'HelloWorldPhp'.
-cd ./HelloWorldPhp
-
-# Create a package.tgz from the source files
-tar -cvzf ./package.tgz --exclude='.DS_Store' --exclude='.git' ./helloworldphp
+# Create a package.tgz from the contents of the package folder
+cd ./HelloWorldPhp/package
+tar -cvzf ../package.tgz --exclude='.DS_Store' --exclude='.git' *
 
 # Create the spk
-# -prevent including of ./helloworldphp in HelloWorldPhp.spk (./package.tgz contains these files)
-tar -cvf ../HelloWorldPhp.spk --exclude='helloworldphp' *
+# -prevent including the contents of ./helloworldphp in HelloWorldPhp.spk (./package.tgz contains these files)
+cd ..
+tar -cvf ../HelloWorldPhp.spk --exclude='package' *
 
 # Clean up
 rm ./package.tgz
